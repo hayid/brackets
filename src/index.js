8df3fe6d5fd648292672673ac00959bdd22d6710
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-27 12:40:51 +0500

    feat: test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let option = bracketsConfig.map(item => item.join(''));
+  let testItem = "";
+  let sub = str;
+
+  for (let j = 0; j < str.length; j++) {
+      for (let i = 0; i < option.length; i++) {
+          if (sub.includes(option[i])) { 
+              testItem = option[i];
+
+              sub = sub.replace(testItem, "");
+              i--;
+          }
+      }
+  }
+  if (sub === '') return true;
+   else return false;
+};
